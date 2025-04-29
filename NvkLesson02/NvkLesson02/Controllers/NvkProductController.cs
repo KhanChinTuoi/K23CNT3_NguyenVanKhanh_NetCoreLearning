05552791aba4d87de37c8c76c684e804238d4ad7
Admin <khanhvank9t727@gmail.com>
2025-04-29 21:21:35 +0700

    nvk-netcore

---
+++ NvkLesson02/NvkLesson02/Controllers/NvkProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NvkLesson02.Controllers
+{
+    public class NvkProductController : Controller
+    {
+        public IActionResult NvkIndex()
+        {
+            ViewData["messageData"] = "Du lieu tu viewData";
+            ViewBag.messageData = "Du lieu tu ViewBag";
+            TempData["messageData"] = "Du lieu TempData";
+            return View();
+        }
+    }
+}
