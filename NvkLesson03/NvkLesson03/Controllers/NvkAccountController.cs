494ea261b86bc6449132c98f35588611a3eb1719
Admin <khanhvank9t727@gmail.com>
2025-05-09 18:57:42 +0700

    nvk-netcore

---
+++ NvkLesson03/NvkLesson03/Controllers/NvkAccountController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NvkLesson03.Models;
+
+namespace NvkLesson03.Controllers
+{
+    public class NvkAccountController : Controller
+    {
+        public IActionResult NvkIndex()
+        {
+            List<NvkAccount> accounts = new List<NvkAccount>
+            {
+                new NvkAccount()
+                {
+                    NvkId = 1, NvkName = "Nguyen Van Khanh",
+                    NvkEmail="khanhvank9t727@gmail.com",
+                    NvkPhone="0984915173",
+                    NvkAddress="Ha Noi",
+                    NvkAvatar= Url.Content("~/Avatar/download.jpg"),
+                    NvkGender=1, NvkBio="idk",
+                    NvkBirthday= new DateTime(2005,5,29)
+                },
+                new NvkAccount()
+                {
+                    NvkId = 2, NvkName = "Nguyen Van Khan",
+                    NvkEmail="khanhvank9t@gmail.com",
+                    NvkPhone="0984915172",
+                    NvkAddress="Ha Noi",
+                    NvkAvatar= Url.Content("~/Avatar/emu.jpg"),
+                    NvkGender=1, NvkBio="idk",
+                    NvkBirthday= new DateTime(2005,4,28)
+                },
+                new NvkAccount()
+                {
+                    NvkId = 3, NvkName = "Nguyen Van Kha",
+                    NvkEmail="khanhvank9t7@gmail.com",
+                    NvkPhone="0984915171",
+                    NvkAddress="Ha Noi",
+                    NvkAvatar= Url.Content("~/Avatar/luka.jpg"),
+                    NvkGender=1, NvkBio="idk",
+                    NvkBirthday= new DateTime(2005,3,27)
+                },
+            };
+            ViewBag.Accounts = accounts;
+            return View();
+        }
+
+        public IActionResult NvkProfile()
+        {
+            NvkAccount account = new NvkAccount()
+            {
+                NvkId = 1,
+                NvkName = "Nguyen Van Khanh",
+                NvkEmail = "khanhvank9t727@gmail.com",
+                NvkPhone = "0984915173",
+                NvkAddress = "Ha Noi",
+                NvkAvatar = "/images/Avatar/download.jpg",
+                NvkGender = 1,
+                NvkBio = "idk",
+                NvkBirthday = new DateTime(2005,5,29)
+            };
+            ViewBag.Accounts = account;
+            return View(); 
+        }
+    }
+}
---
+++ NvkLesson03/NvkLesson03/Models/NvkAccount.cs
@@ -0,0 +1,19 @@
+﻿namespace NvkLesson03.Models
+{
+    public class NvkAccount
+    {
+        public int NvkId { get; set; }
+        public string NvkName { get; set; }
+
+        public string NvkEmail { get; set; }
+        public string NvkPhone { get; set; }
+        public string NvkAvatar { get; set; } 
+        public string NvkAddress { get; set; }
+
+        public string NvkBio { get; set; }
+        public int NvkGender { get; set; }
+
+        public DateTime NvkBirthday { get; set; } 
+
+    }
+}
